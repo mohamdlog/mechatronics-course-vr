1ed140d9e92a57116c33580c1403f5519f0b72bf
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-12-06 14:12:22 -0600

    Complete reset script, fix bugs

--- Assets/Scripts/BoxReset.cs
+++ Assets/Scripts/BoxReset.cs
@@ -3,22 +3,32 @@ using System.Collections;
 
 public class BoxReset : MonoBehaviour
 {
-    public float resetTime = 5f;
-    public GameObject prefab; // Reference to the original prefab
     private Vector3 originalPosition;
+    private Quaternion originalRotation; // Store the original rotation
+    private Transform child;
+    private MeshRenderer meshRenderer;
+    private Collision box;
+    private Renderer renderer;
+    private Material originalMaterial; // Store the original shared material
     private bool isResetting = false; // To prevent multiple coroutines
 
     void Start()
     {
         originalPosition = transform.position;
+        originalRotation = transform.rotation; // Store the original rotation
+        child = transform.GetChild(0);
+        meshRenderer = child.GetComponent<MeshRenderer>();
+        renderer = GetComponent<Renderer>();
+        originalMaterial = renderer.sharedMaterial; // Store the original shared material
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("ConveyorBelt"))
         {
-            if (Vector3.Distance(transform.position, originalPosition) > .1f && !isResetting)
+            if (Vector3.Distance(transform.position, originalPosition) > 0.1f && !isResetting)
             {
+                box = other;
                 StartCoroutine(ResetObject());
             }
         }
@@ -27,16 +37,11 @@ public class BoxReset : MonoBehaviour
     IEnumerator ResetObject()
     {
         isResetting = true;
-
-        // Wait for the specified time
-        yield return new WaitForSeconds(resetTime);
-
-        // Instantiate a new object from the prefab at the original position and rotation
-        Instantiate(prefab, originalPosition, transform.rotation);
-
-        // Destroy the current object
-        Destroy(gameObject);
-
+        yield return new WaitForSeconds(5f);
+        transform.position = originalPosition;
+        transform.rotation = originalRotation; // Reset to the original rotation
+        meshRenderer.enabled = false;
+        renderer.sharedMaterial = originalMaterial; // Reset to the original shared material
         isResetting = false;
     }
 }
